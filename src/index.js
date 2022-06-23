33bc9a89e0f1e6102957f3c30bcf97beea5e0652
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-06-23 16:09:04 -0400

    simplifying code map filter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
+// array of numbers
+
 var numbers = [3, 56, 2, 48, 5];
 
+function double(x) {
+  return x * 2;
+}
 //Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map(double);
+
+console.log(newNumbers);
+// this is essentially a for each loop that does this
+// var newNumbers[];
+
+// function double(x){
+//   newNumbers.push(x*2);
+// }
+// numbers.forEach(double);
+
+// or even simpler
+// var newNumbers = [];
+// numbers.forEach(function (x){
+//   newNumbers.push(x*2);
+// }
+// );
+
+// simplfy the map function as the map function expects a functin - insert an anonymours function
+//map returns an output which is an array (loops through and pushes to an arry)
+const newNumber = numbers.map(function (x) {
+  return x * 2;
+});
+console.log(newNumber);
 
 //Filter - Create a new array by keeping the items that return true.
 
@@ -9,3 +38,8 @@ var numbers = [3, 56, 2, 48, 5];
 //Find - find the first item that matches from an array.
 
 //FindIndex - find the index of the first item that matches.
+const bigNumbers = numbers.filter(function (num) {
+  return num > 10;
+});
+
+console.log(bigNumbers);
